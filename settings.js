b01a956334f8ed0e1ee3e37ff48a6d14e1556427
Rock de Vocht <rock@peter.conz>
2019-09-08 21:39:44 +1200

    reset settings

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@
 // the settings for this application - no trailing / on the base_url please
 settings = {
     // the service layer end-point, change "localhost:8080" to ...
-    base_url: 'https://cloud.simsage.nz/api',
+    base_url: 'https://<server>/api',
     // api version of ws_base
     api_version: 1,
     // web sockets platform endpoint for comms
-    ws_base: 'https://cloud.simsage.nz/ws-api',
+    ws_base: 'https://<server>/ws-api',
     // the organisation's id to search
-    organisationId: "c276f883-e0c8-43ae-9119-df8b7df9c574",
+    organisationId: "<organisation>",
     // the knowledge bases inside this organisation to use
-    kbList: [{name: "demo knowledge base", kbId: "46ff0c75-7938-492c-ab50-442496f5de51", sid: "48f9a7f5-6d6b-9766-a232-6ef59eae7cae"}],
+    kbList: [{name: "<name>", kbId: "<kb>", sid: "<sid>"}],
 };
 
